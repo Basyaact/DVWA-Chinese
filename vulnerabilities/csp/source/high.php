7cb0003f5b90a9eeebd8e894b5bfd2aa8ea7e393
Basyaact <2095657527@qq.com>
2021-08-04 18:30:32 +0800

    fixed typo

--- vulnerabilities/csp/source/high.php
+++ vulnerabilities/csp/source/high.php
@@ -14,7 +14,7 @@ $page[ 'body' ] .= '
 <form name="csp" method="POST">
 	<p>此页面会向 ' . DVWA_WEB_PAGE_TO_ROOT . '/vulnerabilities/csp/source/jsonp.php 发送一个请求并加载代码. 更改页面然后运行你自己的代码.</p>
 	<p>1+2+3+4+5=<span id="answer"></span></p>
-	<input type="button" id="solve" value="求和m" />
+	<input type="button" id="solve" value="求和" />
 </form>
 
 <script src="source/high.js"></script>
